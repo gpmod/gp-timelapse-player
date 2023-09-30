878ea315a04266616c3dc6cc8f9d3dc76dd3e949
gpmod <101346570+gpmod@users.noreply.github.com>
2023-09-30 23:40:37 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cachePrefix = 'gp-player-'
-const cacheVersion = 'v11'
+const cacheVersion = 'v12'
 const cacheName = `${cachePrefix}${cacheVersion}`
 
 const PATHNAME = '/gp-timelapse-player'
